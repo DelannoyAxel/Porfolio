029d211c6eb80c29aeaf7a939587e359358e70f7
DelannoyAxel <delannoy.axel2@gmail.com>
2024-04-26 18:07:01 +0200

    porfolio

--- style.js
+++ style.js
@@ -50,7 +50,42 @@ function alternateText() {
         }, 3000); // Délai de 2 secondes avant de commencer à afficher le texte suivant
     }, speed * texts[index].length + 1000); // Attendre que le texte soit affiché entièrement avant de le cacher
 }
-
-
 alternateText();
+
+
+// modal projets
+
+const btnModal1 = document.querySelector('#btnModal1');
+const btnModal2 = document.querySelector('#btnModal2');
+const modal1 = document.querySelector('#modal1')
+const modal2 = document.querySelector('#modal2')
+const overlay = document.querySelector ('#overlay')
+const closeButtons = document.querySelectorAll('#closeModal');
+
+
+function closeModal() {
+
+    closeButtons.forEach(closeButton => {
+        closeButton.addEventListener('click', () => {
+            const modalId = closeButton.closest('.modal1').id;
+            const modal = document.getElementById(modalId);
+            modal.style.display = 'none';
+            overlay.style.display = 'none';
+        });
+    });
+}
+
+
+
+btnModal1.addEventListener('click', () => {
+modal1.style.display = 'block';
+overlay.style.display = 'block';
+closeModal();
+});
+
+btnModal2.addEventListener('click', () => {
+    modal2.style.display = 'block';
+    overlay.style.display = 'block';
+    closeModal();
+});
 
