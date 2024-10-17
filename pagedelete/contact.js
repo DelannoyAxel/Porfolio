3c617a6fa58d7c67551ccd191905cf3d4aa89fe7
DelannoyAxel <delannoy.axel2@gmail.com>
2024-10-17 11:30:07 +0200

    retouche

--- pagedelete/contact.js
+++
@@ -1,7 +0,0 @@
-const burgerContainer = document.getElementById('burgerContainer');
-const menu = document.getElementById('menu');
-
-burgerContainer.addEventListener('click', function() {
-    burgerMenu.classList.toggle('change');
-    menu.classList.toggle('show');
-});
